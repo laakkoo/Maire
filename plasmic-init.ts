445ee9207c3e31cce860be6efd926e9f7ccabdcb
laakkoo <109277383+laakkoo@users.noreply.github.com>
2022-07-14 13:01:40 +0300

    Update plasmic-init.ts

--- plasmic-init.ts
+++ plasmic-init.ts
@@ -23,3 +23,19 @@ export const PLASMIC = initPlasmicLoader({
 // https://docs.plasmic.app/learn/app-hosting/#set-a-plasmic-project-to-use-your-app-host
 
 // PLASMIC.registerComponent(...);
+import React, { ReactNode } from 'react';
+
+export interface HelloWorldProps {
+  children?: ReactNode;
+  className?: string;
+  verbose?: boolean;
+}
+
+export function HelloWorld({ children, className, verbose }: HelloWorldProps) {
+  return (
+    <div className={className} style={{ padding: '20px' }}>
+      <p>Hello there! {verbose && 'Really nice to meet you!'}</p>
+      <div>{children}</div>
+    </div>
+  );
+}
